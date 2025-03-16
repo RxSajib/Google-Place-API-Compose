0267b7d2cc315a4ad2f04fb9fd8da4f58866e436
Asus <sajibroy206@gmail.com>
2025-03-16 22:45:22 +0600

    add google maps when tap on location name

--- app/src/main/java/com/theme/googleplaceapi/MainActivity.kt
+++ app/src/main/java/com/theme/googleplaceapi/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.theme.googleplaceapi
 
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -12,8 +17,10 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.theme.googleplaceapi.ui.theme.GooglePlaceAPITheme
+import androidx.core.net.toUri
 
 private const val TAG = "MainActivity"
 class MainActivity : ComponentActivity() {
@@ -22,14 +29,30 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             GooglePlaceAPITheme {
+                val context = LocalContext.current
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Box(modifier = Modifier.padding(innerPadding)){
                         PlacesAutoComplete { placeId ->
                             Log.d(TAG, "Selected Place ID: $placeId")
+                            openPlaceInGoogleMaps(context, placeId)
                         }
                     }
                 }
             }
+        }
+    }
+
+    @SuppressLint("QueryPermissionsNeeded")
+    fun openPlaceInGoogleMaps(context: Context, placeId: String) {
+        val gmmIntentUri =
+            "https://www.google.com/maps/search/?api=1&query=place_id:$placeId".toUri()
+        val mapIntent = Intent(Intent.ACTION_VIEW, gmmIntentUri)
+        mapIntent.setPackage("com.google.android.apps.maps") // Google Maps অ্যাপ ওপেন করবে
+
+        if (mapIntent.resolveActivity(context.packageManager) != null) {
+            context.startActivity(mapIntent)
+        } else {
+            Toast.makeText(context, "Google Maps অ্যাপ পাওয়া যায়নি!", Toast.LENGTH_SHORT).show()
         }
     }
 }
